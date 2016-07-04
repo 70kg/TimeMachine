50889df82f91919626497d756ba38e8f94ac51bd
drakeet <drakeet.me@gmail.com>
2016-07-04 22:56:02 +0800

    Added YOUDAO_TRANSLATE_KEY_FROM

--- transformer/src/main/java/me/drakeet/transformer/BuildVars.java
+++ transformer/src/main/java/me/drakeet/transformer/BuildVars.java
@@ -7,4 +7,5 @@ public class BuildVars {
 
     //obtain your own KEY at http://fanyi.youdao.com/openapi
     public static String YOUDAO_TRANSLATE_KEY = "";
+    public static String YOUDAO_TRANSLATE_KEY_FROM = "";
 }
