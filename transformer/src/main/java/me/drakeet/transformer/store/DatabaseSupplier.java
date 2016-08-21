c30a80e873221f81a22fcf05a54711ed2817395d
drakeet <drakeet.me@gmail.com>
2016-08-21 23:13:16 +0800

    Updated MultiType to v1.2.1 and added CONTENT_DESC_COLUMN to db
    - Added searchContent to MessageStore
    - Recreated the database

--- transformer/src/main/java/me/drakeet/transformer/store/DatabaseSupplier.java
+++ transformer/src/main/java/me/drakeet/transformer/store/DatabaseSupplier.java
@@ -15,6 +15,7 @@ final class DatabaseSupplier extends SqlDatabaseSupplier {
 
     static final String ID_COLUMN = "id";
     static final String CONTENT_COLUMN = "content";
+    static final String CONTENT_DESC_COLUMN = "contentDesc";
     static final String FROM_USER_ID_COLUMN = "fromUserId";
     static final String TO_USER_ID_COLUMN = "toUserId";
     static final String CREATED_AT_COLUMN = "createdAt";
@@ -24,11 +25,12 @@ final class DatabaseSupplier extends SqlDatabaseSupplier {
     private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE
         + " (" + ID_COLUMN + " VARCHAR(32) PRIMARY KEY, "
         + CONTENT_COLUMN + " BLOB, "
+        + CONTENT_DESC_COLUMN + " VARCHAR(128), "
         + FROM_USER_ID_COLUMN + " VARCHAR(32), "
         + TO_USER_ID_COLUMN + " VARCHAR(32), "
-        + CREATED_AT_COLUMN + " INTEGER); ";
+        + CREATED_AT_COLUMN + " INTEGER);";
 
-    private static final String DATABASE_NAME = "Messages";
+    private static final String DATABASE_NAME = "Messages.db";
     private static final int VERSION = 1;
 
 
