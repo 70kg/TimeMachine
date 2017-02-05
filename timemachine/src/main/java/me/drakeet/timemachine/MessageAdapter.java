1e6b253f31373cdd1250efc1fc98c0ba5b5d316c
drakeet <drakeet.me@gmail.com>
2017-02-05 14:59:14 +0800

    Upgrade & adapted multitype to v2.4.1 (#42)
    * Upgrade & adapted multitype to v2.4.1
    
    * Added build-tools-25.0.2 to travis

--- timemachine/src/main/java/me/drakeet/timemachine/MessageAdapter.java
+++ timemachine/src/main/java/me/drakeet/timemachine/MessageAdapter.java
@@ -2,6 +2,7 @@ package me.drakeet.timemachine;
 
 import android.support.annotation.NonNull;
 import java.util.List;
+import me.drakeet.multitype.FlatTypeClassAdapter;
 import me.drakeet.multitype.MultiTypeAdapter;
 
 /**
@@ -11,10 +12,14 @@ public class MessageAdapter extends MultiTypeAdapter {
 
     public MessageAdapter(@NonNull List<Message> messages) {
         super(messages);
+        setFlatTypeAdapter(new MessageFlattenAdapter());
     }
 
 
-    @NonNull @Override public Class onFlattenClass(@NonNull Object message) {
-        return ((Message) message).content.getClass();
+    private static class MessageFlattenAdapter extends FlatTypeClassAdapter {
+
+        @NonNull @Override public Class onFlattenClass(@NonNull Object message) {
+            return ((Message) message).content.getClass();
+        }
     }
 }
